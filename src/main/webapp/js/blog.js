9737ecc91a04e734bda3c1b795d0b1af7b07ccf1
nguyenbeo <nguyenbeo@gmail.com>
2015-11-16 13:08:22 +0100

    Added demo of build-in directives

--- src/main/webapp/js/blog.js
+++ src/main/webapp/js/blog.js
@@ -7,28 +7,32 @@
 	    	"author": "Tester 1",
 	    	"content": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. ",
 	    	"date": "2015-11-14",
-	    	"isPublished" : true
+	    	"isPublished" : true,
+	    	"noOfVisits" : 1000
 	    },
 	    {
 	    	"title": "Donec pede justo, fringilla vel",
 	    	"author": "Tester 2",
 	    	"content": "Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt",
 	    	"date": "2015-11-15",
-	    	"isPublished" : true
+	    	"isPublished" : true,
+	    	"noOfVisits" : 2000
 	    },
 	    {
 	    	"title": "Etiam rhoncus. Maecenas tempus",
 	    	"author": "Tester 3",
 	    	"content": "Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.",
 	    	"date": "2015-11-16",
-	    	"isPublished" : true
+	    	"isPublished" : true,
+	    	"noOfVisits" : 3000
 	    },
 	    {
 	    	"title": "Etiam rhoncus",
 	    	"author": "Tester 4",
 	    	"content": "Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.",
 	    	"date": "2015-11-16",
-	    	"isPublished" : false
+	    	"isPublished" : false,
+	    	"noOfVisits" : 0
 	    }
 	];
 
@@ -38,5 +42,17 @@
 	
 	app.controller('PostController', function() {
 		this.articles = articles;
+	});
+	
+	app.controller('PanelController', function() {
+		this.tab = 1;
+		
+		this.selectTab = function(setTab) {
+			this.tab = setTab;
+		};
+		
+		this.isSelected = function(checkTab) {
+			return this.tab === checkTab;
+		}
 	});
 })();
